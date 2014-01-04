f8276867f766dede403178d8fa0c922b51a70445
DL <444c43@gmail.com>
2014-01-03 19:10:51 -0500

    initial setup
    for reference, command line:
    rails _3.2.8_ new . --git --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dl::Application.config.secret_token = '6d4fedfe879e446c9ae88596a221dbb0525aaf17354ef75a2e0e5f0b85cf04c4817f94781daac24ec9e5a35d7a87b21a0bc54911cfcd046f230a7f5301f172c2'
