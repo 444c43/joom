9dcb7af04040da38df68c8e53434a11596426286
DL <444c43@gmail.com>
2014-01-06 22:25:15 -0500

    refactor photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,21 @@
 class Photo
+
   def self.all
+    album_photos
+  end
+
+  private
+
+  def self.client
     client = Picasa::Client.new(user_id: "mrmicahcooper@gmail.com")
+  end
+
+  def self.album
     album = client.album.show("5797040163555140801")
+  end
+
+  def self.album_photos
     album.photos.reverse.map{|p| p.content.src }
   end
+  
 end
