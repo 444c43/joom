0a2d5c36b1499c5ac1a2fb8c372f339e6f1a775a
DL <444c43@gmail.com>
2014-01-11 15:21:22 -0500

    wip slugging all around

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,18 @@
+require 'active_record_helper'
+require 'models/post'
+
+describe Post do
+  it "requires a title" do
+    Post.new.should have(1).errors_on(:title)
+  end
+
+  describe "#build_slug" do
+    it "it sets the slug as the parameterized title" do
+      post = Post.new(title: '2toneDL')
+      post.build_slug
+      post.slug.should eq('dl')
+    end
+  end
+
+
+end
