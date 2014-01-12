4c9c827fc61c6c1c08d37e68929e73ab3a660868
DL <444c43@gmail.com>
2014-01-12 10:20:59 -0500

    update Gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -22,12 +22,6 @@ ActiveRecord::Schema.define(:version => 20140110221421) do
     t.datetime "updated_at",      :null => false
   end
 
-  create_table "contacts", :force => true do |t|
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "posts", :force => true do |t|
     t.text     "body"
     t.boolean  "published"
