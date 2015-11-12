771b19b3adeb0d9dd1fbfd1a58419bd26c36bd2c
DL <444c43@gmail.com>
2015-11-11 19:36:13 -0500

    update config files, rails 4 upgrade

--- config/application.rb
+++ config/application.rb
@@ -12,24 +12,18 @@ end
 module Joom
   class Application < Rails::Application
     config.encoding = "utf-8"
-
     config.filter_parameters += [:password]
-
     config.active_support.escape_html_entities_in_json = true
-
-    config.active_record.whitelist_attributes = true
-
     config.assets.enabled = true
-
     config.assets.compile = true
-
     config.assets.version = '1.0'
+    config.eager_load = true
+    config.serve_static_files = true
 
     config.generators do |g|
       g.test_framework :rspec, fixture: true, views: false
       g.fixture_replacement :fabrication, dir: "spec/fabricators"
       g.integration_tool :rspec
     end
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,19 @@
 Joom::Application.routes.draw do
 
-  match 'ui(/:action)', controller: 'ui'
   root to: 'posts#index'
-
-  get '/sign_in', to: 'sessions#new', as: :sign_in
-  post '/sign_in', to: 'sessions#create', as: :sign_in
-  get '/sign_out', to: 'sessions#destroy', as: :sign_out
-  get '/photos/', to: 'photos#index', as: :photos
 
   resources :posts
   resources :blog, controller: :posts
-  get '/blog', to: 'posts#index', as: :blog_home
+
+  get   '/sign_in', to: 'sessions#new', as: :sign_in
+  post  '/sign_in', to: 'sessions#create', as: :create_session
+
+  get   '/sign_out', to: 'sessions#destroy', as: :sign_out
+
+  get   '/blog', to: 'posts#index', as: :blog_home
   # get '/blog/:id', to: 'pots#show', as: :blog
   # get '/blog/:id/edit', to: 'posts#edit', as: :blog
   # put '/blog/:id', to: 'posts#update', as: :blog
 
-
-  resources :posts
-
   get '/heartbeat', to: proc { [200, {}, ''.chars] }
-
 end
