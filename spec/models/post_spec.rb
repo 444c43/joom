057272eb5c982fe7a23a3b28d59d3d2549637936
DL <444c43@gmail.com>
2015-11-17 22:37:26 -0500

    first passing test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,21 +1,12 @@
-require 'active_record_helper'
+require 'spec_helper'
 require 'post'
 
 describe Post do
-  it "requires a title" do
-    expect(Post.new).to have(1).error_on(:title)
-  end
-
-  it "requires unique title" do
-    Post.create(title: 'A Unique Title')
-    expect(Post.new(title: 'A Unique Title')).to have(1).errors_on(:title)
-  end
-
   describe "#build_slug" do
     it "it sets the slug as the parameterized title" do
-      post = Post.new(title: '2toneDL')
+      post = Post.new(title: 'A New Post')
       post.build_slug
-      expect(post.slug).to eq('2toneDL')
+      expect(post.slug).to eq('a-new-post')
     end
   end
 end
