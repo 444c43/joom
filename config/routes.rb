d760be8bd61ab6ae9963d3c154c099e0a50e9123
DL <444c43@gmail.com>
2015-11-21 20:47:23 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,7 @@ Joom::Application.routes.draw do
   resources :posts
   resources :blog, controller: :posts
 
-  get   '/sign_in', to: 'sessions#new', as: :sign_in
-  post  '/sign_in', to: 'sessions#create', as: :create_session
-
-  get   '/sign_out', to: 'sessions#destroy', as: :sign_out
-
-  get   '/blog', to: 'posts#index', as: :blog_home
-  # get '/blog/:id', to: 'pots#show', as: :blog
-  # get '/blog/:id/edit', to: 'posts#edit', as: :blog
-  # put '/blog/:id', to: 'posts#update', as: :blog
-
-  get '/heartbeat', to: proc { [200, {}, ''.chars] }
+  get  '/sign_in',  to: 'sessions#new',     as: :sign_in
+  post '/sign_in',  to: 'sessions#create',  as: :create_session
+  get  '/sign_out', to: 'sessions#destroy', as: :sign_out
 end
