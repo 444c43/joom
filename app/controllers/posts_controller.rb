099296dfa54e65cd1dbc22e5981886510c39770a
DL <444c43@gmail.com>
2015-11-30 21:31:11 -0500

    update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,13 @@
 class PostsController < ApplicationController
   before_filter :require_admin, except: [:index, :show]
 
-  expose(:posts)
+  expose(:posts) { get_posts }
   expose(:published_posts) { Post.published }
   expose(:post, finder: :find_by_slug_or_id, attributes: :post_attributes)
 
-  def index
-    @posts = admin_signed_in? ? posts : published_posts
-  end
-
-  def show
-    @posts = admin_signed_in? ? posts : published_posts
-  end
-
   def create
     if post.save
-      redirect_to :blog_home
+      redirect_to root_path 
     else
       render :new
     end
@@ -35,6 +27,11 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def get_posts
+    available_posts = admin_signed_in? ? Post.all : published_posts
+    available_posts.paginate(:page => params[:page], :per_page => 2)
+  end
 
   def post_attributes
     params.require(:post).permit(
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
 
   expose(:admin) { Admin.where(email: params[:email].downcase).first }
+  expose(:posts) { get_posts }
+  expose(:published_posts) { Post.published }
 
   def create
     if admin && admin.authenticate(params[:password])
@@ -13,6 +15,12 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out_admin
-    redirect_to :sign_in
+    redirect_to root_path
+  end
+
+  private
+
+  def get_posts
+    admin_signed_in? ? Post.all : published_posts
   end
 end
