f9e4276d18aedd61c1a3a9340a14e8200d1f66a4
DL <444c43@gmail.com>
2015-12-05 22:44:01 -0500

    fix post search order

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   private
 
   def get_recent_posts
-    Post.all.published.where(id: 2..4)
+    Post.published.order('created_at DESC').where(id: 2..4)
   end
 
   def deny_admin_access
