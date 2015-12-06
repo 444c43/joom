bfc69068cf7fad65acfe063a71fe6e9cb8a149c4
DL <444c43@gmail.com>
2015-12-05 21:12:18 -0500

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Joom::Application.routes.draw do
   resources :posts
   resources :blog, controller: :posts
 
+  get  '/about',    to: 'pages#about',      as: :about
   get  '/sign_in',  to: 'sessions#new',     as: :sign_in
   post '/sign_in',  to: 'sessions#create',  as: :create_session
   get  '/sign_out', to: 'sessions#destroy', as: :sign_out
