21e9b103f0e2881bb2833e184d7c43010fc09ea8
DL <444c43@gmail.com>
2015-12-05 21:59:22 -0500

    refactor controllers, remove old assets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 class ApplicationController < ActionController::Base
 
   authem_for :admin
-
   protect_from_forgery
+
+  expose(:recent_posts) { get_recent_posts }
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   private
+
+  def get_recent_posts
+    Post.all.published.find(2,3,4)
+  end
 
   def deny_admin_access
     redirect_to :sign_in unless admin_signed_in?
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,4 @@
 class PagesController < ApplicationController
-
-  expose(:admin) { Admin.where(email: params[:email].downcase).first }
-  expose(:recent_posts) { get_recent_posts }
-  expose(:posts) { get_posts }
-  expose(:published_posts) { Post.published }
-
   def about
   end
-
-private
-
-  def get_posts
-    admin_signed_in? ? Post.all : published_posts
-  end
-
-  def get_recent_posts
-    Post.all.published.limit(3)
-  end
-
 end
