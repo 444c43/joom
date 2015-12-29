e41d9e6ed337cd63e33e84186c6c05e3a69594d6
DL <444c43@gmail.com>
2015-12-28 23:16:12 -0500

    update post tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,4 +9,28 @@ describe Post do
       expect(post.slug).to eq('a-new-post')
     end
   end
+
+  context 'add tags to post' do
+    describe "existing post" do
+      it "adds a tag" do
+        post = create :post
+        post.tag_list.add("one")
+        post.save!
+
+        post.reload
+
+        expect(post.tag_list).to eq(["one"])
+      end
+    end
+
+    describe "new post" do
+      it "adds a tag" do
+        post = create :post, tag_list: ["one"]
+
+        post.reload
+
+        expect(post.tag_list).to eq(["one"])
+      end
+    end
+  end
 end
