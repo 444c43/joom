3a0c9394804940fa152815f24d288d053bbdc325
DL <444c43@gmail.com>
2016-01-27 09:40:24 -0500

    resolving syntax highlighting, WIP

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,15 +1,6 @@
 module PostsHelper
-
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
-                                       autolink: true, 
-                                       space_after_headers: true, 
-                                       fenced_code_blocks: true,
-                                       strikethrough: true,
-                                       superscript: true,
-                                       footnotes: true,
-                                       disable_indented_code_blocks: true,
-                                      )
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, redcarpet_options)
     text = markdown.render(text)
     syntax_highlighter(text).html_safe
   end
@@ -21,14 +12,25 @@ module PostsHelper
 
   private
 
-  def syntax_highlighter(html)
-    doc = Nokogiri::HTML.fragment(html)
-    doc.search("pre").each do |pre|
-      if (code = pre.child).name == 'code'
-        pre.replace Pygmentize.process(code.text.rstrip, code[:class])
-      end
-    end
-    doc.to_html
+  def redcarpet_options
+    {
+      autolink:                     true,
+      fenced_code_blocks:           true,
+      disable_indented_code_blocks: true,
+      footnotes:                    true,
+      space_after_headers:          true,
+      strikethrough:                true,
+      superscript:                  true,
+    }
   end
 
+  def syntax_highlighter(html)
+    doc = Nokogiri::HTML.fragment(html)
+    #doc.search("pre").each do |pre|
+    #  if (code = pre.child).name == 'code'
+    #    pre.replace Pygmentize.process(code.text.rstrip, code[:class])
+    #  end
+    #end
+    doc.to_html
+  end
 end
