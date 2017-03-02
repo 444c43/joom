e2ee473fa27bed27f306662c4c68932af970dc78
DL <444c43@gmail.com>
2017-03-02 11:02:02 -0500

    use index page instead of about

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Joom::Application.routes.draw do
 
-  root to: 'posts#index'
+  root to: 'pages#index'
 
   resources :posts
   resources :blog, controller: :posts
 
-  get  '/about',    to: 'pages#about',      as: :about
   get  '/sign_in',  to: 'sessions#new',     as: :sign_in
   post '/sign_in',  to: 'sessions#create',  as: :create_session
   get  '/sign_out', to: 'sessions#destroy', as: :sign_out
